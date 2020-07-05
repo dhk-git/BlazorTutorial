93a2befba5ae721580fa79efd617050f8b3e3209
dhkwak <dreamaboutme.k@gmail.com>
2020-07-05 22:46:21 +0900

    mouse move, footer show/hide

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,11 @@ namespace EmployeeManagement.Web.Pages
     {
         public Employee Employee { get; set; } = new Employee();
 
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
         [Inject]
         public IEmployeeService EmployeeService{ get; set; }
 
@@ -22,6 +28,25 @@ namespace EmployeeManagement.Web.Pages
         {
             Id ??= "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        //protected void Mouse_Move(MouseEventArgs e)
+        //{
+        //    Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        //}
+
+        protected void Button_Click() {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+
         }
     }
 }
