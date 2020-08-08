430563e63510fc0532a25e479825ef60200fc718
dhkwak <dreamaboutme.k@gmail.com>
2020-08-08 15:51:44 +0900

    Blazor EventCallback https://www.youtube.com/watch?v=Kb5bWCuQ9bQ&list=PL6n9fhu94yhVowClAs8-6nYnfsOTma14P&index=28

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -23,8 +23,22 @@ namespace EmployeeManagement.Web.Pages
             //return base.OnInitializedAsync();
             //await Task.Run(LoadEmployees);
             Employees = (await EmployeeService.GetEmployees()).ToList();
-
         }
+
+        protected int SelectedEmployeesCount { get; set; } = 0;
+
+        protected void EmployeeSelectionChanged(bool isSelected)
+        {
+            if (isSelected)
+            {
+                SelectedEmployeesCount++;
+            }
+            else
+            {
+                SelectedEmployeesCount--;
+            }
+        }
+
 
         //private void LoadEmployees()
         //{
