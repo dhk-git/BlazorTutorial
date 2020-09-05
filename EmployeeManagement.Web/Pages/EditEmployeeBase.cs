6c8b99d780e1d7c3045b42c526a9a6e06e64dfd7
dhkwak <dreamaboutme.k@gmail.com>
2020-09-05 19:20:56 +0900

    삭제 Confirm 추가 Delete confirmation dialog in Blazor https://www.youtube.com/watch?v=Caw5hmq4dEY&list=PL6n9fhu94yhVowClAs8-6nYnfsOTma14P&index=45

--- EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -93,11 +93,27 @@ namespace EmployeeManagement.Web.Pages
             }
         }
 
-        protected async Task Delete_Click()
+        protected ProgimTech.Components.ConfirmBase DeleteConfirmation { get; set; }
+
+        protected void Delete_Click()
         {
-            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
-            NavigationManager.NavigateTo("/");
+            DeleteConfirmation.Show();
         }
+
+        protected async Task ConfirmDelete_Click(bool deleteConfirmed)
+        {
+            if (deleteConfirmed)
+            {
+                await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+                NavigationManager.NavigateTo("/");
+            }
+        }
+
+        //protected async Task Delete_Click()
+        //{
+        //    await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+        //    NavigationManager.NavigateTo("/");
+        //}
 
     }
 }
---
+++ ProgimTech.Components/ConfirmBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgimTech.Components
+{
+    public class ConfirmBase : ComponentBase
+    {
+        protected bool ShowConfirmation { get; set; }
+        
+        [Parameter]
+        public string ConfirmationTitle { get; set; } = "Delete Confirmation";
+        
+        [Parameter]
+        public string ConfirmationMessage { get; set; } = "Are you sure you want to delete?";
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged(); //Re rendering
+        }
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        protected async Task OnConfirmationChange(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+    }
+}
