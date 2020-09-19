816b249e14cc00ec7f9f7edfc41c2c0d7feacd50
dhkwak <dreamaboutme.k@gmail.com>
2020-09-19 21:31:19 +0900

    ASP NET core identity setup in blazor application https://www.youtube.com/watch?v=FZzwhivKrtU&list=PL6n9fhu94yhVowClAs8-6nYnfsOTma14P&index=51

---
+++ EmployeeManagement.Api/Migrations/20200919090158_AddIdentitySupport.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Api.Migrations
+{
+    public partial class AddIdentitySupport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "Employees",
+                newName: "LastName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Employees",
+                newName: "lastName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
